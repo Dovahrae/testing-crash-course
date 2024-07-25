53affff12be39b977d29ebb16802b3fb272278be
Dovahrae <167934960+Dovahrae@users.noreply.github.com>
2024-07-25 19:45:41 -0400

    eh?

--- console-assert/src/validation.js
+++ console-assert/src/validation.js
@@ -1,6 +1,36 @@
 export const validatePassword = (password) => {
-    return true;
-}
+    if (password.length < 8) {
+        return false;
+    }
+
+    const common = ["12345678", "password", "admin123", "abc12345", "secretsz"];
+
+    for (let item of common) {
+        if (password === item) {
+            return false;
+        }
+    }
+
+    const specialCharacters = ["!", "@", "#"];
+    for (let specialCharacter of specialCharacters) {
+        const index = password.indexOf(specialCharacter);
+        if (index >= 0) {
+            return true;
+        }
+    }
+    return false;
+};
+
+console.assert(
+    validatePassword("a1b2c3d4e5!") === true,
+    "this is a valid password"
+);
+console.assert(validatePassword("a") === false, "this is too short");
+console.assert(
+    validatePassword("anything") === false,
+    "there is no special character"
+);
+console.assert(validatePassword("12345678") === false, "too common");
 
 // What the code should do
 // 1. check that the password is >7 characters in length
