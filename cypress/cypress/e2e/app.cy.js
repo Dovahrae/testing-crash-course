263572a0fb2f5a9afbd97b97094f714f3e7907c9
Dovahrae <167934960+Dovahrae@users.noreply.github.com>
2024-07-25 20:07:14 -0400

    learning cypress

--- cypress/cypress/e2e/app.cy.js
+++ cypress/cypress/e2e/app.cy.js
@@ -1,4 +1,15 @@
 describe("Password Validator", () => {
+    it("checks for a good password", () => {
+        cy.visit("http://localhost:3000");
+        cy.get("#password").type("kn1ttedpumk!n");
+        cy.get('[data-cy="login"]').click();
+        cy.get('[data-cy="valid-password"]');
+    });
+
     it("checks for a short password", () => {
-    })
-})
+        cy.visit("http://localhost:3000");
+        cy.get("#password").type("kn1ttedpumk!n");
+        cy.get('[data-cy="login"]').click();
+        cy.get('[data-cy="valid-password"]');
+    });
+});
